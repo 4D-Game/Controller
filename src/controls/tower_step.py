9c89d277b3c2618649fb148d71a2147e3c9e83ba
lukson83 <Sonja.Lukas@edu.fh-joanneum.at>
2021-11-22 21:27:45 +0100

    new tower_step.py

---
+++ src/controls/tower_step.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+Created: 11/22/21
+by: Sonja Lukas
+Tower control with 2 stepper
+Game part
+"""
+
+#from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+from ..hardware.tower_stepHAL import TowerHAL
+
+import time
+import atexit 
+import logging
+import mqtt
+
+class Tower(): 
+    """
+        Gamecontroll Tower
+
+    """
+    def __init__(self):
+        pass
+
+
+
+    async def shutdown(self):
+        pass
+
